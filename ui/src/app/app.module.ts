04894359be9b027663be584a4ff9559986c1f0d2
fardeen ahmad <fardeen.cs@gmail.com>
2020-03-06 22:24:20 +0530

    cod eupdated

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy, CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -17,9 +17,9 @@ import { AppComponent } from './app.component';
 // Import containers
 import { DefaultLayoutComponent } from './containers';
 
-import { P404Component } from './views/error/404.component';
-import { P500Component } from './views/error/500.component';
-import { LoginComponent } from './views/login/login.component';
+// import { P404Component } from './views/error/404.component';
+// import { P500Component } from './views/error/500.component';
+// import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 
 const APP_CONTAINERS = [
@@ -36,45 +36,47 @@ import {
 
 // Import routing module
 import { AppRoutingModule } from './app.routing';
+import { LoginComponent } from './containers/modules/login/login.component';
 
 // Import 3rd party components
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ChartsModule } from 'ng2-charts';
-import { FeaturesModule } from './containers/modules/features/features.module';
-import { AdminModule } from './containers/modules/admin/admin.module';
-import { LoginModule } from './containers/modules/login/login.module';
+// import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+// import { TabsModule } from 'ngx-bootstrap/tabs';
+// import { ChartsModule } from 'ng2-charts';
+// import { FeaturesModule } from './containers/modules/features/features.module';
+// import { AdminModule } from './containers/modules/admin/admin.module';
+// import { LoginModule } from './containers/modules/login/login.module';
 // import { UserLoginComponent } from './dump/user-login/user-login.component';
 // import { UserRoleComponent } from './dump/user-role/user-role.component';
 
 
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    AppAsideModule,
-    AppBreadcrumbModule.forRoot(),
-    AppFooterModule,
-    AppHeaderModule,
-    AppSidebarModule,
-    PerfectScrollbarModule,
-    BsDropdownModule.forRoot(),
-    TabsModule.forRoot(),
-    ChartsModule,
-    LoginModule,
+    // AppAsideModule,
+    // AppBreadcrumbModule.forRoot(),
+    // AppFooterModule,
+    // AppHeaderModule,
+    // AppSidebarModule,
+    // PerfectScrollbarModule,
+    // BsDropdownModule.forRoot(),
+    // TabsModule.forRoot(),
+    // ChartsModule,
+    // LoginModule,
     // FeaturesModule,
     // AdminModule
   ],
   declarations: [
     AppComponent,
-    ...APP_CONTAINERS,
-    P404Component,
-    P500Component,
+    // ...APP_CONTAINERS,
+    // P404Component,
+    // P500Component,
     LoginComponent,
-    RegisterComponent,
+    // RegisterComponent,
     // UserLoginComponent,
     // UserRoleComponent
   ],
